aa4d8b44ff8bbe6a16c41d3b79f65b090f623706
Edgar Nazyrov <nazyrov01@gmail.com>
2023-03-04 10:08:10 +0500

    Initial Commit

---
+++ uunitschedule/uunitscheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  uunitscheduleApp.swift
+//  uunitschedule
+//
+//  Created by Эдгар Назыров on 04.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct uunitscheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
